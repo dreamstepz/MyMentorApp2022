dd61cd0d394fe9f590181d06f2d45bc0b94ec8d7
dreamstepz <dreamstepztech@gmail.com>
2022-02-01 11:55:12 +0530

    Initial

--- MyMentorApp/Model/LoginModel.cs
+++ MyMentorApp/Model/LoginModel.cs
@@ -9,5 +9,6 @@ namespace MyMentorApp.Model
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string ConfimPassword { get; set; }
     }
 }
