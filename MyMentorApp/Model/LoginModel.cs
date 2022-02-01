54e6bcd2ff28bf02e5b5d0e24262e3d6d12ff0ff
dreamstepz <dreamstepztech@gmail.com>
2022-02-01 12:02:01 +0530

    Added Comment

--- MyMentorApp/Model/LoginModel.cs
+++ MyMentorApp/Model/LoginModel.cs
@@ -7,6 +7,7 @@ namespace MyMentorApp.Model
 {
     public class LoginModel
     {
+        //Added the Development Branch
         public string UserName { get; set; }
         public string Password { get; set; }
         public string ConfimPassword { get; set; }
